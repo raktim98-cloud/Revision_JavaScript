9cf29d26742ea17ecb2c2ea12f9b85bcdfc9fb63
Raktim Shuvrow <shuvrow98@gmail.com>
2025-08-12 04:21:06 +0600

    learn datatype

---
+++ 01_Basic/01_basic.js
@@ -0,0 +1,12 @@
+const accountId = "1221"
+let accountEmail = "raktim@gmail.com"
+var accountPass = "110012"
+accountDetails = "Dhaka"
+
+accountPass = "shuvrow@.com"
+
+console.table([accountId,accountEmail,accountPass,accountDetails])
+
+// console.log(accountEmail);
+
+
---
+++ 01_Basic/01_dataType.js
@@ -0,0 +1,13 @@
+// DataType
+
+let myName = "Raktim" //String
+let age = "26" //Number
+// "Boolean" true/false
+// "null" Its means kind of object,maximum person says its mistake of Script
+// "Object"
+// "Undefine"
+// "Symble"
+
+
+// console.log(typeof undefined)
+console.log(typeof null)
